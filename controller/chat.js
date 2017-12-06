acab7070d2772e21b9036836d2a278779523a405
dimpl3 <dimpl3@users.noreply.github.com>
2017-12-05 17:34:00 -0800

    got private messaging working

--- controller/chat.js
+++ controller/chat.js
@@ -4,22 +4,60 @@ module.exports = function (io) {
 /**
  * Chat console message.
  */
- io.on('connection', function(socket){
-   socket.on('game chat', function(msg){
-     io.emit('game chat', msg);
-   });
- });
+ // io.on('connection', function(socket){
+ //   socket.on('game chat', function(msg){
+ //     io.emit('game chat', msg);
+ //   });
+ // });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//   });
+// });
 
-io.on('connection', function(socket){
-  console.log('a user connected: ' + socket.id);
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  io.on('connection', function(socket) {
+    console.log('User Connected (Server Side - Lobby Chat): ', socket.id);
+    socket.on('lobbyChat', function(msg){
+      io.emit('lobbyChat', msg);
+    });
+
+    socket.on('disconnect', function(){
+      console.log('User Disconnected (Server Side - Lobby Chat)');
+    });
+
   });
-});
+
+  var nsp = io.of('/game');
+
+  nsp.on('connection', function(socket) {
+    console.log('User Connected (Server Side - Game Room): ', socket.id);
+
+    // socket.on('gameChat', function(msg){
+    //   nsp.emit('gameChat', msg);
+    // });
+
+
+    socket.on('join', (params, callback) => {
+      console.log('Game Player Name: ' , params.name);
+      console.log('Room Name: ', params.room);
+      console.log('Game Socket ID: ', socket.id);
+
+      socket.join(params.room);
+
+      socket.on(params.room, function(msg){
+        console.log('TEST: ', params.room);
+        console.log('TEST MESSAGE: ', msg);
+        nsp.emit(params.room, msg);
+      });
+
+      callback();
+    });
+
+    socket.on('disconnect', function(){
+      console.log('User Disconnected (Server Side - Game Room)');
+    });
+
+  });
+
 };

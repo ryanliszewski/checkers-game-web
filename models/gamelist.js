7c7bdfde5ec955f4be9daab97357b8f42b39ab38
Sukhjit Singh <moni@sfs-wifi-dhcp-10-143-245-191.wifi.sfsu.edu>
2017-12-05 19:27:54 -0800

    added the gameList database model

---
+++ models/gamelist.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var gameList = sequelize.define('gameList', {
+    gameId: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return gameList;
+};

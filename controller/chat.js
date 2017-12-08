4da94299f0e4cdf8456efdd462f76df54d0c772e
dimpl3 <dimpl3@users.noreply.github.com>
2017-12-07 21:51:17 -0800

    Added username to lobby gamelist

--- controller/chat.js
+++ controller/chat.js
@@ -26,7 +26,7 @@ module.exports = function (io) {
       io.emit('lobbyChat', msg);
     });
 
-    gameList.findAll({attributes: ['gameId', 'isGameFull']})
+    gameList.findAll({attributes: ['gameId', 'isGameFull', 'gameCreator']})
       .then(results => {
         // console.log('Game IDs: ', JSON.stringify(results))
         gameArray = JSON.stringify(results);

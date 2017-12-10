3d1580b35b75acc14f3df4badfc196708d127838
dimpl3 <dimpl3@users.noreply.github.com>
2017-12-09 22:47:27 -0800

    removed game from db when user leaves and its the only player in the game

--- controller/chat.js
+++ controller/chat.js
@@ -1,10 +1,11 @@
 const gameList = require('../models').gameList;
 const lobbyChat = require('../models').lobbyChat;
 const sequelize = require('sequelize');
+
 function dbCreateMessage(msgObj) {
   lobbyChat.create({
-        username: msgObj.username,
-        message: msgObj.message
+      username: msgObj.username,
+      message: msgObj.message
     })
     .then(results => {})
     .catch(err => {
@@ -13,10 +14,12 @@ function dbCreateMessage(msgObj) {
 }
 
 function dbCreateGame(params) {
-  gameList.findOrCreate({ where: {
-      gameId: params.gameID,
-      gameCreator: params.name
-    }})
+  gameList.findOrCreate({
+      where: {
+        gameId: params.gameID,
+        gameCreator: params.name
+      }
+    })
     .then(results => {})
     .catch(err => {
       console.log(err)
@@ -26,6 +29,7 @@ function dbCreateGame(params) {
 function dbDestroyGame(params) {
   gameList.destroy({
       where: {
+        isGameFull: 'false',
         gameId: params.gameID
       }
     })
@@ -55,8 +59,13 @@ module.exports = function(io) {
   io.on('connection', function(socket) {
     console.log('User Connected (Server Side - Lobby Chat): ', socket.id);
 
-    lobbyChat.findAll({ order: [['id', 'DESC']], limit:10}).then(results => {
-      for(let i = results.length - 1; i >= 0; i--) {
+    lobbyChat.findAll({
+      order: [
+        ['id', 'DESC']
+      ],
+      limit: 10
+    }).then(results => {
+      for (let i = results.length - 1; i >= 0; i--) {
         io.to(socket.id).emit('lobbyChat', results[i]['dataValues']['username'] + ": " + results[i]['dataValues']['message']);
       }
     })
@@ -93,28 +102,8 @@ module.exports = function(io) {
         if (params.isGameFull == 'true') {
           dbGameFull(params)
         } else {
-          console.log("GAME: ", params.gameID);
-          console.log("GAME STATUS:", params.isGameFull);
           dbCreateGame(params);
-          // dbDestroyGame(params);
-          // dbCreateGame(params);
         }
-      });
-
-      gameList.findAll({
-          attributes: ['gameId', 'isGameFull', 'gameCreator']
-        })
-        .then(results => {
-          gameArray = JSON.stringify(results);
-        })
-        .catch(err => {
-          console.log(err)
-        });
-
-      socket.emit('gameListActive', JSON.stringify(gameArray));
-
-      socket.on('gameMove', function(msgObj) {
-        io.emit('gameMove', msgObj);
       });
 
       socket.broadcast.to(params.gameID).emit(params.gameID, `Player ${params.name} has joined.`);
@@ -122,26 +111,13 @@ module.exports = function(io) {
         nsp.emit(params.gameID, msg);
       });
 
-      /*
-            socket.leave(params.gameID, () => {
-              gameList.destroy({ where: { gameId: params.gameID } })
-                .then(results => {
-                  console.log('UDATED GAME LIST AFTER LEAVING ROOM:', JSON.stringify(results))
-                })
-                .catch(err => {
-                  console.log(err)
-                })
-            })
-      */
+      socket.on('disconnect', function() {
+        console.log('User Disconnected GAME (Server Side)');
+        dbDestroyGame(params);
+      });
+
       callback();
     });
-
-    socket.on('disconnect', function() {
-      // socket.on('leave', (params, callback) => {
-      //   socket.broadcast.to(params.gameID).emit(params.gameID, `Player ${params.name} has left game.`);
-      //   console.log('User LEFT Game (Server Side)');
-      // });
-    }); // NSP Disconnected
 
   }); // NSP Connection
 
